b70b04d4866b68b944f3f100ef92364f36ba6f5d
apolon23 <polybotko23@gmail.com>
2018-02-19 16:13:22 +0200

    Authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from "@angular/http";
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
+import { UsersService } from './shared/services/users.service';
+import { AuthService } from './shared/services/auth.service';
+
 
 
 @NgModule({
@@ -10,9 +16,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    AuthModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [UsersService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
