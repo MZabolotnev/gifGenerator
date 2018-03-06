10adfe5d04763f0dee3fa6b90c5f05d9da1b7d45
MZabolotnev <miron.zabolotniev@gmail.com>
2018-03-06 19:52:30 +0200

    test branch

---
+++ src/app/shared/directives/dn-d.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DnDDirective } from './dn-d.directive';
+
+describe('DnDDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DnDDirective();
+    expect(directive).toBeTruthy();
+  });
+});
